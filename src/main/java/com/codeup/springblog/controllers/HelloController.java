50073baa3dad09aae336f75b17f70ca6f15e30de
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-07-20 15:57:18 -0500

    Beginning of Thymeleaf

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -10,16 +11,33 @@ public class HelloController {
     public String hello(){
         return "<h1>Hello from Spring!</h1>";
     }
+
     @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String sayHello(@PathVariable String name) {
-        return "Hello " + name + "!";
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
     }
+
+    @GetMapping("/join")
+    public String showJoinForm(){
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model){
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
+    }
+
+
+
+
     @GetMapping("/number/{num}")
     @ResponseBody
     public int displayNumber(@PathVariable int num){
         return num;
     }
+
     @RequestMapping(path = "hello/in/{color}", method = RequestMethod.GET)
     @ResponseBody
     public String helloInColor(@PathVariable String color){
--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
     public String homeLanding() {
-        return "<h1>This is the landing page!";
+        return "home";
     }
 
 }
